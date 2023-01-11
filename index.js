b6e51ddc9338164189cd818f30178f46aef8484a
Sophie-Zulinski <sophie.zulinski@gmx.at>
2023-01-11 16:57:03 +0100

    Added comments in index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const website = "https://memegen-link-examples-upleveled.netlify.app/"
+
+
+//const imgSrcs = ? filter everything in the htmlContent that is a string including the string "memgen"
+
+//const arrayTenMemes cut the imgSrc to the first 10 element
+//maybe?: const arrayTenMemes = imgSrcs.filter(function (i) {return i<10})
+//--> get a new array with only 10 elements (each of these elemtents will look similar this: "https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300")
+// --> fetch/loop? over these images and get the jpeg data (which format is this??)
+// --> save the jpeg data in folders (e.g. data of first image into 01.jpeg-folder etc.), maybe with the command touch??
+
+
+
+
+
+//Old versions:
+
+//First sanity check:
+// console.log("hello")
+
+// Second try fetching data from website via crawler
+//import Crawler from 'node-html-crawler';
+//const crawler = new Crawler('website');
+//console.log(crawler)
+
